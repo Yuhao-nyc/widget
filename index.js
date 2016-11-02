11927769ad29102a791dd85e13ff757a88b06714
Yuhao <jimfengyh@gmail.com>
2016-11-02 17:25:42 -0400

    routes setted up, with 2 widget pages inside landing page now

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 var express = require('express');
+var router = express.Router();
 var app = express();
 var path = require('path');
 
-app.use('/app', express.static('app'));
+app.listen(3000, function () {
+  console.log('my app is listening on port 3000!');
+});
 
+//using static files (css, js, png etc)
+app.use('/app', express.static('./app'));
+app.use('/charts', express.static('./charts'));
+app.use('/news', express.static('./news'));
+
+//routing
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+//news routing
+app.get('/news', function(req, res) {
+    res.sendFile(path.join(__dirname + '/news/index.html'));
+});
+
+//charts rounting
+app.get('/charts', function(req, res) {
+    res.sendFile(path.join(__dirname + '/charts/index.html'));
+});
+
+//404
+app.use(function(req, res, next) {
+  res.status(404).sendFile(__dirname + '/404.html')
+});
+
+//error handle
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('<h1>:( Looks like something broke!</h1>');
 });
